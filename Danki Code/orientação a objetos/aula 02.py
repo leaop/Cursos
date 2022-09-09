2a146d578cea340d3b7187c0b38ac1d9ddaf0124
leaop <leaoneto_7@hotmail.com>
2022-09-09 10:59:16 -0300

    Create aula 02.py

---
+++ Danki Code/orientação a objetos/aula 02.py
@@ -0,0 +1,7 @@
+# a ideia de hoje é fazer o mesmo registro da aula passada(pacientes) com programacao orientada a objetos
+# logica de relacao entre funcao e variável
+"""Classe, objeto e construtor"""
+class Paciente:
+# Paciente - conjunto de objetos que compartilham de caracteristicas similares
+    pass
+
