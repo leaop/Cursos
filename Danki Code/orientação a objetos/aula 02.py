6544f1976263aa1bf912bfe2a134bace28284877
leaop <leaoneto_7@hotmail.com>
2022-09-11 15:38:39 -0300

    11.09.22

--- Danki Code/orientação a objetos/aula 02.py
+++ Danki Code/orientação a objetos/aula 02.py
@@ -4,4 +4,7 @@
 class Paciente:
 # Paciente - conjunto de objetos que compartilham de caracteristicas similares
     pass
-
+#O construtor é uma função com o mesmo nome da classe que é criado internamente pelo python
+#Como eu sei que foi criado? pela linha de código: <paciente.Pciente object at 0x7fc65739bb0
+#esse ultimo código é uma linha de memória
+# instanciar objeto =  passar o valor do objeto para um variável, tendo maior controle sobre ela
