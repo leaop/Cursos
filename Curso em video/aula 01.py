ee800de33c55be5693ed7bfc081bce7e48091afe
Leão Neto <111656788+leaop@users.noreply.github.com>
2022-10-06 16:59:16 -0300

    alterações da aula 4

--- Curso em video/aula 01.py
+++ Curso em video/aula 01.py
@@ -1,2 +1,22 @@
 print('7' + '4')
 print(7+4)
+#variáveis = espaço na memoria que podem guardar coisas
+idade = 25
+peso = 70
+print(idade, peso)
+#o sinal de + se limita a aparacer variáveis do mesmo tipo
+print(idade + peso)
+#recurso de interação
+nome = input("qual o seu nome?")
+#desafio
+#o nome de uma pessoa e mostre uma interação com esse nome
+print(f'Bem vindo, {nome}')#minha opção
+#interacao com a data de nascimento
+dia = input('Dia:' )
+mes = input('Mes:' )
+ano = input('Ano:' )
+print(f'Voce nasceu no dia {dia} de {mes} de {ano}')
+# um script que leia dois numeros e mostre a soma entre eles
+primeiro_numero = input('Primeiro numero = ')
+segundo_numero = input('Segundo numero = ')
+print(int(primeiro_numero) + int(segundo_numero))
