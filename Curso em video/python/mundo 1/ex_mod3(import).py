613dbf08ed38604447845229fa829ef595db9a2f
Leão Neto <111656788+leaop@users.noreply.github.com>
2022-10-23 10:34:16 -0300

    pequenos ajustes

--- Curso em video/python/mundo 1/ex_mod3(import).py
+++ Curso em video/python/mundo 1/ex_mod3(import).py
@@ -46,4 +46,3 @@ pygame.mixer.music.load('nome do arquivo')
 pygame.mixer.music.play()
 pygame.event.wait()
 
-
---
+++ Curso em video/python/mundo 1/ex_mod3(texto).py
@@ -0,0 +1,4 @@
+#nome completo com letas maiusculas, minusculas e a quantidade de letras do primeiro nome
+nome = input('Qual seu nome completo? ')#o input por padrão gera uma lista
+print('Analisando seu nome...')
+print('Seu nome completo em maiusculas é {}'.format(nome.upper()))
