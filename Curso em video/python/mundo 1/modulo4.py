2dbc5f433f60b8a826f1a973c083d39e31a2e71f
Leão Neto <111656788+leaop@users.noreply.github.com>
2022-11-02 12:03:39 -0300

    modulo condicional

---
+++ Curso em video/python/mundo 1/modulo4.py
@@ -0,0 +1,18 @@
+#condicionais (if, else)
+"""
+O que precis entender é o seguinte dos condiconais
+if x:
+    condicionante verdadeira
+else:
+    condicionante falsa
+
+o resto segue a identação
+alem dessa forma temos: print('Parabens' if: condição else: 'Precisa melhorar')
+"""
+
+#faça o computador pensar em um numero inteiro entre 1 e 5, para que o usuário descubra (anunciar se venceu ou perdeu)
+#escreva um programa que leia a velocidade de um carro , se passar disso vai ter multa de 7 reais para cada km
+#crie um que mostre se é par ou impar
+#Pergunte quanto vai ser a extensao da viagem, se for até 200km, de o preco de 0,50 por km e se passar, vais er 0,45 por km
+#
+#calcule se as medidas das retas podem formar um triangulo
