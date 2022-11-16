f72b47e434ee4734894d1ac6c28363637b8468fe
Leão Neto <111656788+leaop@users.noreply.github.com>
2022-11-16 05:43:46 -0300

    inserir se quiser

---
+++ Curso em video/python/mundo 1/cores.py
@@ -0,0 +1,16 @@
+#codigos para estilo
+'''
+0 = none
+1 =  bold
+2 = underline
+7 = negative
+'''
+#cores do texto
+'''vao do 30 ao 37 e para ter mais tem que instalar biblioteca'''
+
+#cores de fundo
+'''o normal também é limitado entre 40 e 47'''
+
+print('\033[4;30;40mOlá, mundo!\033[m')
+
+#o 033[m abre e fecha o espaço que quer colocar a cor
