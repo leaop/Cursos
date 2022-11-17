3bb6a0605cc5f3569c0dffb09ea30bb3d3ba9cdf
Leão Neto <111656788+leaop@users.noreply.github.com>
2022-11-17 18:44:01 -0300

    save

---
+++ Curso em video/python/mundo 2/exercicio_38.py
@@ -0,0 +1,8 @@
+n1 = int(input('Primeiro número: '))
+n2 = int(input('Segundo núnemro: '))
+if n1 > n2:
+    print('O primeiro é maior')
+elif n1 == n2:
+    print('Os dois são iguais')
+elif n1 < n2:
+    print('O segundo é maior')
