0f250ddb1988b88b09f09ddd901b0071ad04458b
Leão Neto <111656788+leaop@users.noreply.github.com>
2023-07-21 23:34:54 -0300

    Semana 1

---
+++ EBAC/comentarios.c
@@ -0,0 +1,18 @@
+#include <stdio.h> // conumicacao com o usuário
+#include <stdlib.h> // alocacao de espaço na memória
+#include <locale.h> //alocacao de texto
+
+
+int main()
+{
+    int opcao=0;//definindo variáveis
+    setlocale(LC_ALL, "Portuguese");//linguagem definida
+    print("### Cartorio da EBAC###/n/n");//inicio do menu
+    printf("Escolha a opcao de desejada no menu:\n\n");
+    printf("\t1 - Registrar nomes\n");
+    printf("\t2 - Consultar nomes\n");
+    printf("\t3 - Deletar nomes\n\n");
+    printf("Opção final:");// final do menu
+
+
+}
